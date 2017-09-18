d2a90bdadb22a4f19bce1b7314585a0978bc1005
DipuKumar <32012773+D2epu@users.noreply.github.com>
2017-09-18 13:06:20 +0530

    Update vowel.asm

--- vowel.asm
+++ vowel.asm
@@ -17,30 +17,30 @@ start:
 	
 	push	msg
 	call	printf
-	add		esp, 4
+	add	esp, 4
 	
 	lea  	eax, [ebp - 4]
 	
 	push	eax
 	push	ifmt
 	call	scanf
-	add		esp, 8
+	add	esp, 8
 	
 	push	dword [ebp - 4]
 	call	isvowel
-	add		esp, 4
+	add	esp, 4
 	
-	cmp		eax, 0
-	jz		_notvowel
+	cmp	eax, 0
+	jz	_notvowel
 	
 	push	vfmt
 	call	printf
-	add		esp, 4
-	jmp		_endif
+	add	esp, 4
+	jmp	_endif
 _notvowel:
 	push	nfmt
 	call	printf
-	add		esp, 4
+	add	esp, 4
 _endif:	
 	leave
 	ret
@@ -48,26 +48,26 @@ _endif:
 isvowel:
 	enter	0, 0
 	
-	cmp		dword [ebp + 8], 'a'
-	jz		_done
+	cmp	dword [ebp + 8], 'a'
+	jz	_done
 	
-	cmp		dword [ebp + 8], 'e'
-	jz		_done
+	cmp	dword [ebp + 8], 'e'
+	jz	_done
 	
-	cmp		dword [ebp + 8], 'i'
-	jz		_done
+	cmp	dword [ebp + 8], 'i'
+	jz	_done
 	
-	cmp		dword [ebp + 8], 'o'
-	jz		_done
+	jz	dword [ebp + 8], 'o'	
+	jz      _done
 	
-	cmp		dword [ebp + 8], 'u'
-	jz		_done
+	cmp	dword [ebp + 8], 'u'
+	jz	_done
 	
-	mov		eax, 0
+	mov	eax, 0
 	leave	
 	ret
 _done:
-	mov		eax, 1
+	mov	eax, 1
 	leave
 	ret
 
